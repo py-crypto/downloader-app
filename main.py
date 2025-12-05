a50aa0be9433115d74b3943b6db6611a01dac5f9
py-crypto <pycoder89@gmail.com>
2025-12-05 17:24:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ url=st.text_input('URL:')
 
 #url='https://www.youtube.com/watch?v=hm8b1Gnw1Z4'
 if st.button('download'):
-	st.success('downloading...')
 	ydl_opts = {
-        "format": "96",  # MUST be a single progressive stream
-        "outtmpl": "%(title)s.%(ext)s",
+        'list_formats':True,
     }
 		
 	with yt_dlp.YoutubeDL(ydl_opts) as yd1:
-		yd1.download([url])
-	st.success('downloaded.')
+		info=yd1.extract_info(url)
+
+st.text(info['formats'])
